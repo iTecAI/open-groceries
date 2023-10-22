3256625671da88170c6550b343592ab2d7ae7b0d
Dax Harris <matteovh@gmail.com>
2023-10-22 10:21:51 -0400

    wegmans location acquisition

--- __init__.py
+++ __init__.py
@@ -7,10 +7,5 @@ from adapters import *
 
 
 if __name__ == "__main__":
-    """weg = Wegmans()
-    print(weg.search_groceries("meatballs"))"""
-
-    costco = Costco()
-    #print(costco.search_groceries("meatballs"))
-    #print(costco.get_grocery_item("1454123"))
-    print(costco.get_locations("14623"))
+    weg = Wegmans()
+    print(weg.get_locations("Rochester Institute Of Technology"))
--- models/location.py
+++ models/location.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from math import radians, cos, sin, asin, sqrt
 
 @dataclass
 class LatLong:
@@ -15,6 +16,18 @@ class LatLong:
             return LatLong(latitude=float(data[1]), longitude=float(data[0]))
         else:
             return LatLong(latitude=float(data[0]), longitude=float(data[1]))
+        
+    def distance_to(self, other: "LatLong") -> float:
+        # convert decimal degrees to radians
+        lat1, long1, lat2, long2 = map(radians, [self.latitude, self.longitude, other.latitude, other.longitude])
+        # haversine formula
+        dlon = long2 - long1
+        dlat = lat2 - lat1
+        a = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
+        c = 2 * asin(sqrt(a))
+        # Radius of earth in kilometers is 6371
+        km = 6371 * c
+        return km
         
 @dataclass
 class Address:
